5311cfc8c589ab21dea91d1c89654fd4414e815b
StanSNS <stanimirsergev159@gmail.com>
2024-03-28 15:41:36 +0200

    add manual xml invocation.

--- src/main/java/gym/backend/controller/ScrapeDataFromWeb.java
+++ src/main/java/gym/backend/controller/ScrapeDataFromWeb.java
@@ -1,15 +1,19 @@
 package gym.backend.controller;
 
+import gym.backend.init.ProductDataFromSheetInit;
 import gym.backend.init.ProductDataFromWebSite;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class ScrapeDataFromWeb {
 
     private final ProductDataFromWebSite productDataFromWebSite;
+    private final ProductDataFromSheetInit productDataFromSheetInit;
 
 
     @GetMapping("/startCollecting")
@@ -17,5 +21,12 @@ public class ScrapeDataFromWeb {
         System.out.println("Start collecting data...");
 
         productDataFromWebSite.addEnemyPrices();
+    }
+
+    @GetMapping("/getXML")
+    public void startXML() throws IOException {
+        System.out.println("Start collecting data...");
+
+        productDataFromSheetInit.startInit();
     }
 }
