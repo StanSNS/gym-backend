0127fdd8b7e5f36f1afa9946d62f43c1a25923d2
StanSNS <stanimirsergev159@gmail.com>
2024-08-04 10:47:41 +0300

    test which part of retrieving data does not work on production

--- src/main/java/gym/backend/controller/User/AboutDataController.java
+++ src/main/java/gym/backend/controller/User/AboutDataController.java
@@ -1,14 +1,20 @@
 package gym.backend.controller.User;
 
+import gym.backend.init.Products.ProductDataFromSheetInit;
+import gym.backend.init.initService.RequestService;
 import gym.backend.models.DTO.AboutDataDto;
 import gym.backend.service.AboutDataService;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 import static gym.backend.consts.Urls.UserControllerUrlPaths.ABOUT_DATA;
 
@@ -19,9 +25,26 @@ import static gym.backend.consts.Urls.UserControllerUrlPaths.ABOUT_DATA;
 public class AboutDataController {
 
     private final AboutDataService aboutDataService;
+    private final ProductDataFromSheetInit productDataFromSheetInit;
+    private final RequestService requestService;
 
     @GetMapping(ABOUT_DATA)
     public ResponseEntity<AboutDataDto> getAllAboutData() {
         return new ResponseEntity<>(aboutDataService.getAboutData(), HttpStatus.OK);
+    }
+
+    @GetMapping("test")
+    public ResponseEntity<String> test() throws IOException {
+        XSSFSheet sheet = requestService.getDistroSheet();
+        if (sheet == null) {
+            return new ResponseEntity<>("Sheet is empty", HttpStatus.OK);
+        }
+
+        XSSFRow row = sheet.getRow(sheet.getFirstRowNum() + 1);
+        if (row == null) {
+            return new ResponseEntity<>("Row is empty", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("All passed ?", HttpStatus.OK);
     }
 }
--- src/main/java/gym/backend/security/SpringSecurityConfig.java
+++ src/main/java/gym/backend/security/SpringSecurityConfig.java
@@ -46,7 +46,8 @@ public class SpringSecurityConfig implements WebMvcConfigurer {
                     SEND_EMAIL,
                     HOME,
                     PRODUCT,
-                    ABOUT_DATA
+                    ABOUT_DATA,
+                    "test"
             ).permitAll();
 
             authorize.requestMatchers(
